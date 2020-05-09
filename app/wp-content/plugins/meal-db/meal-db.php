6ddb0072b6e44d70aff412b1a9bd86bc7d587ad8
Tentative <micoli.giacomo@gmail.com>
2020-05-10 00:10:41 +0200

    plugin scaffolding

---
+++ app/wp-content/plugins/meal-db/meal-db.php
@@ -0,0 +1,12 @@
+/*
+    Plugin Name: Meal DB
+    Plugin URI: https://www.enovathemes.com
+    Description: Display meals from Meal DB
+    Author: Tentative
+    Version: 1.0
+    Author URI: http://enovathemes.com
+*/
+ 
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
